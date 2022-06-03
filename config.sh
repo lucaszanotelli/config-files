d900431d3020058bb8913ef39a661deeef44cad4
Lucas Zanotelli <lucaszanotelli@gmail.com>
2022-06-03 07:14:40 -0300

    correct source path

--- config.sh
+++ config.sh
@@ -6,5 +6,5 @@ mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/
 cat .bash_profile >> ~/.bash_profile
 cat .bash_aliases >> ~/.bash_aliases
-source .bash_profile
-source .bash_aliases
+source ~/.bash_profile
+source ~/.bash_aliases
