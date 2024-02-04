40ebb6cfc2531aa36fe2e7e602b532fd9bfc5a7e
lucas zanotelli <lucas@zanotelli.dev>
2024-02-04 13:58:13 -0300

    move version control to user root directory

--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p $HOME/.config/nvim
-cp init.vim $HOME/.config/nvim/
-cat .bash_profile >> $HOME/.bash_profile
-cat .bash_aliases >> $HOME/.bash_aliases
-source $HOME/.bash_profile
-source $HOME/.bash_aliases
